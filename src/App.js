615d2df633530129f99c7d8c711a095ddefa52b1
Harshal Vinod Jadhav <138389903+Harshalll07@users.noreply.github.com>
2024-04-30 00:43:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// import myImage from '.C:\Users\Rushikesh jadhav\OneDrive\Desktop\APi\portfolio\IMG-20230101-WA0013.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="body">
+
+    
+
+    <div className="Port">
+      <p>Hello I'm Harshal Jadhav</p>
+    </div>
+
+    <section data-animate="true" >
+    <div className="myself">
+   <p> A computer science student from India.<br />I aim to deepen my <br />understanding in various areas<br /> of technology and product development.</p>
+   <p className="project">Projects...</p>
+    </div>
+    <div>
+
+    </div>
+
+    </section>
+    
+
+    </div>
+  );
+}
+
+export default App;
